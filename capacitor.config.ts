be800ab26bda632c0c66bee29658fdde645ee416
Daniel O. Enamudu <oshionefa@gmail.com>
2024-01-24 19:57:07 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Access Bank (Successful Transfer)',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
